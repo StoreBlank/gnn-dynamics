c4df7a384ded80a40e2c49347a96b306808b5dde
Boey-li <baoyul2@illinois.edu>
2024-04-10 12:15:25 -0500

    change utils directory to utils_script.

--- src/utils_script/data_merge.py
+++ src/utils_script/data_merge.py
@@ -11,8 +11,10 @@ def merge_data(main_folders, destination_path, material_categories):
     # collect all subfolders and their corresponding material
     for i, folder in enumerate(main_folders):
         for subfolder in os.listdir(folder):
+            if subfolder.endswith(".npy"):
+                continue # skip the camera info
             subfolders_info.append((os.path.join(folder, subfolder), material_categories[i]))
-    
+        
     # shuffle the subfolders information list
     np.random.shuffle(subfolders_info)
     
@@ -23,14 +25,14 @@ def merge_data(main_folders, destination_path, material_categories):
         # os.makedirs(new_subfolder_path, exist_ok=False)
         
         # copy the entire subfolder to the new destination with a new name
-        print(f"Copying {subfolder_path} to {new_subfolder_path}")
+        print(f"Copying {subfolder_path} to {new_subfolder_path}...")
         shutil.copytree(subfolder_path, new_subfolder_path)
         
         # create a file to store the material category
         with open(os.path.join(new_subfolder_path, "material.txt"), "w") as f:
             f.write(material)
         
-        print(f"Done with {new_subfolder_path}")
+        print(f"Done with {new_subfolder_path} with material {material}.")
 
 if __name__ == "__main__":
     main_folders = [
